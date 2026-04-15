5e279ce99f34df110cd144a5bcbb0f13fc22eb2a
JC <joelconkle@gmail.com>
2026-04-15 16:45:01 +0200

    conductor update

--- analysis_datasets/02_codes/1_execute_conductor.r
+++ analysis_datasets/02_codes/1_execute_conductor.r
@@ -25,17 +25,21 @@ if (!exists("projectFolder", envir = .GlobalEnv) || !exists("analysisCodes", env
   source(file.path(getwd(), "profile_OSE-DA-NT.R"))
 }
 
-source(file.path(analysisCodes, "2_build_cmrs2_series.r"))
 source(file.path(analysisCodes, "2_build_cmrs2_bw.r"))
-source(file.path(analysisCodes, "2_build_cmrs2_iod.r"))
-source(file.path(analysisCodes, "2_build_cmrs2_ant.r"))
-source(file.path(analysisCodes, "2_build_cmrs2_iycf.r"))
+source(file.path(analysisCodes, "3_preferred_bw.r"))
 
- source(file.path(analysisCodes, "3_preferred_series.r"))
- source(file.path(analysisCodes, "3_preferred_bw.r"))
- source(file.path(analysisCodes, "3_preferred_iod.r"))
- source(file.path(analysisCodes, "3_preferred_ant.r"))
- source(file.path(analysisCodes, "3_preferred_iycf.r"))
+source(file.path(analysisCodes, "2_build_cmrs2_iod.r"))
+source(file.path(analysisCodes, "3_preferred_iod.r"))
+
+source(file.path(analysisCodes, "2_build_cmrs2_series.r"))
+source(file.path(analysisCodes, "3_preferred_series.r"))
+
+source(file.path(analysisCodes, "2_build_cmrs2_ant.r"))
+source(file.path(analysisCodes, "3_preferred_ant.r"))
+
+source(file.path(analysisCodes, "2_build_cmrs2_iycf.r"))
+source(file.path(analysisCodes, "3_preferred_iycf.r"))
+
 
 # --- Optional post-build validation ---
 # Set run_verify <- TRUE before sourcing conductor to enable.
